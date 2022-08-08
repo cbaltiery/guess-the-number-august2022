b99b2ea5384bf95f86e30d71bd0b50d6b7c8c293
Chester Altiery <cbaltiery@gmail.com>
2022-08-08 00:21:09 -0400

    third commit

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
+const prompt = require("prompt-sync")({ sigint: true});
 const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
+const { isTypedArray } = require("util/types");
+
+const readlineInterface = readline.createInterface(
+    process.stdin, 
+    process.stdout
+    );
 
 function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+    return new Promise((resolve, reject) => {
+        readlineInterface.question(questionText, resolve)
+    })
 }
-const num = Math.ceil(Math.random() * 100);
+let randomNum = Math.ceil(Math.random() * 100)
+let H ="higher"
+let L = "lower"
+let C = "correct"
 
 
+async function start() { 
+  let promptIntro= await ask("Hi, let's play a game!\nPlease type in a number from 1 to 100 and I will try and guess it!: ");
+  promptIntro = parseFloat(promptIntro)  
+  return promptGuess();
+}  
+ async function promptGuess() {
+  let guessNumber= await ask("Is your number..."+randomNum+"?")
+  let response = await ask ("\nPlease let me know if it was higher(H), lower(L) or if I was correct(C)?")
 
+    if(response === C) {
+      console.log(" \nYesssss you got it!!  The number was " + guessNumber + " Congrats!")
+    process.exit()
+    } else if (response === L){
+      console.log("No it is actually lower than that!")
+      return promptGuess()
+    } else if (response === H){
+      console.log("No it's actually higher than that!")
+      return promptGuess()
+    }
 
-async function start() {
-  console.log ("Hello, do you want to play a game?");
-  let response = await ask("Yes or No?");
-  while ((response != "No"||"no"||"N"||"n") && (response != "Yes"||"yes"||"y"||"Y")){
-  } if (response != "No"||"no"||"N"||"n"){
-    console.log("Good-Bye")
-  } else if (response = "Yes"||"yes"||"y"||"Y"){
-  console.log("OK!...Please think of a number between 1 and 100.\n I will try and guess it!")
-  let randomNumber = num
-  console.log(randomNumber)
-  promtGuess(randomNumber)
-  }
 }
-  async function promptGuess(answer) {
-  let guessNumber = await ask("What is your guess?\nI won't peek, I swear...\n");
-  console.log('Your guess is: ' + guessNumber);
-  
-
-
-
-
-
-
-  
-
-
- process.exit();
-}
-
 start();
